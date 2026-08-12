59d39c68b7a73e7794b044e6b636213e00c07d25
Char <73309364+CharGoldenYT@users.noreply.github.com>
2026-08-12 02:49:03 -0400

    and YOU gotta help us!

--- src/main.py
+++ src/main.py
@@ -17,15 +17,6 @@ app = f.FastAPI(lifespan=lifespan)
 app.include_router(fr.router)
 
 if __name__ == "__main__":
-    try:
-        open("test-repo/.gitignore", 'r')
-        print("HEY YOU STILL HAD A DIRECTORY")
-
-        import subprocess as sp
-        sp.run(["rm", "-rf", "test-repo"], check=True)
-    except: print("All good to go!")
-    
-    
     with open("token.txt", "r") as f:
         token = f.read().strip()
         if token == "INPUT_DISCORD_TOKEN_FOR_YOUR_CLANKER":
@@ -35,6 +26,17 @@ if __name__ == "__main__":
             print("token.txt is empty or not set (how tf)")
             sys.exit(1)
         else:
+
+            try:
+                file = open("test-repo/test-text.txt", 'w') # this WILL trip it.
+                file.write("FAILURE")
+                file.close()
+                print("HEY YOU STILL HAD A DIRECTORY")
+
+                import subprocess as sp
+                sp.run(["rm", "-rf", "test-repo"], check=True)
+            except Exception as e: print("All good to go! `" + string(e) + "`")
+
             print("valid. go ahead, cheif!")
             print(''.join('*' if char != '.' else '.' for char in token))
     uvicorn.run(app, host="0.0.0.0", port=8000)
